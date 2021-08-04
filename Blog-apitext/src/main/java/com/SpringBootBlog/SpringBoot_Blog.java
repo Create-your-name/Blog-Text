0e5bf6789244a9ebc533ed4497773fdfec845cc3
刘海 <2464206128@qq.com>
2021-08-04 15:37:20 +0800

    难以言喻的体验

--- Blog-apitext/src/main/java/com/SpringBootBlog/SpringBoot_Blog.java
+++ Blog-apitext/src/main/java/com/SpringBootBlog/SpringBoot_Blog.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan(value = "com.SpringBootBlog.dao")
 public class SpringBoot_Blog {
     public static void main(String[] args) {

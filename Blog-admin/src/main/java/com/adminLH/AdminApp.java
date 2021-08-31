d8dc5f48ae23df9f652f7ce045418f3cfa5481e1
刘海 <2464206128@qq.com>
2021-08-31 17:22:22 +0800

    后台的 增删改查

--- Blog-admin/src/main/java/com/adminLH/AdminApp.java
+++ Blog-admin/src/main/java/com/adminLH/AdminApp.java
@@ -10,4 +10,5 @@ public class AdminApp {
     public static void main(String[] args) {
         SpringApplication.run(AdminApp.class,args);
     }
+
 }
---
+++ Blog-admin/src/main/java/com/adminLH/mapper/PermissionMapper.java
@@ -0,0 +1,11 @@
+package com.adminLH.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.adminLH.pojo.Permission;
+
+import java.util.List;
+
+public interface PermissionMapper extends BaseMapper<Permission> {
+
+
+}
